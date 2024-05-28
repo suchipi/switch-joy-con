7d9713086512dade681610f5d5c087d0867f5b07
Lily Skye <me@suchipi.com>
2024-05-28 16:28:03 -0600

    add manual side override for use with third-party joycons

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ function listConnectedJoyCons() {
     .filter(device => device.vendorId === 1406)
     .map(device => {
       return Object.assign({}, device, {
-        open() {
-          if (device.productId === 8198) {
+        open(optionalSideOverride) {
+          if (device.productId === 8198 || optionalSideOverride === "left") {
             return new JoyConLeft(device.path);
-          } else if (device.productId === 8199) {
+          } else if (
+            device.productId === 8199 ||
+            optionalSideOverride === "right"
+          ) {
             return new JoyConRight(device.path);
           } else {
-            throw new Error("Unknown Joy-Con model");
+            throw new Error(
+              "Unknown Joy-Con model '" +
+                device.vendorId +
+                ":" +
+                device.productId +
+                "'; cannot auto-detect whether it's a left-side or right-side Joy-Con. Pass either 'left' or 'right' as an argument to the open() method."
+            );
           }
         }
       });
